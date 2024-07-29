047caa5b5bf89e2db47ca8e7b081f8387a94ea1f
MillerFelix <millerredigolo@gmail.com>
2024-07-29 15:17:32 -0300

    Final adjustments

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header className={styles.header}>
       <Link to={"/"}>
         <span>Miller_Felix</span>
       </Link>
-      <nav>
+      <nav
+        className={`${styles.menuSandwich} ${showMenu ? styles.show : ""}`}
+        onClick={toggleMenu}
+      >
         <Link to="/">Home</Link>
         <Link to="/about">Sobre</Link>
         <Link to="/projects">Projetos</Link>
         <Link to="/contacts">Contatos</Link>
       </nav>
+      <div className={styles.menuButton} onClick={toggleMenu}>
+        <span className={styles.row}></span>
+        <span className={styles.row}></span>
+        <span className={styles.row}></span>
+      </div>
     </header>
   );
 }
--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -2,13 +2,13 @@ import styles from "./PageNotFound.module.css";
 
 function PageNotFound() {
   return (
-    <>
+    <section className={styles.page404}>
       <h2 className={styles.title}>Algo deu errado!</h2>
       <div className={styles.texts}>
         <span className={styles.big_text}>404</span>
         <strong className={styles.red_text}>Página não Localizada!</strong>
       </div>
-    </>
+    </section>
   );
 }
 
